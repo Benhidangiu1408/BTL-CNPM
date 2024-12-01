6a75495c824766808276978dc009974fcb4c353e
huuphuoc1218 <huuphuoc1218@gmail.com>
2024-12-01 23:46:56 +0700

    Update Signin.tsx

--- src/pages/SignInPage/Signin.tsx
+++ src/pages/SignInPage/Signin.tsx
@@ -1,4 +1,4 @@
-import /*React,*/ { useState } from "react";
+import { /*React,*/ useState } from "react";
 import "./style.css";
 
 const Signin = () => {
@@ -10,49 +10,51 @@ const Signin = () => {
 
   return (
     <>
-      <div className="login-container">
-        <div className="login-box">
-          <button className="back-button">←</button>
-          <h2>ĐĂNG NHẬP</h2>
-          <form>
-            <div className="input-group">
-              <input
-                type="text"
-                placeholder="Tên đăng nhập"
-                className="input-field"
-              />
-            </div>
-            <div className="input-group">
-              <input
-                type={passwordVisible ? "text" : "password"}
-                placeholder="Mật khẩu"
-                className="input-field"
-              />
-              <button
-                type="button"
-                className="toggle-password"
-                onClick={togglePasswordVisibility}
-              ></button>
-            </div>
-            <button className="login-button" type="submit">
-              ĐĂNG NHẬP
-            </button>
-            <div className="remember">
-              <input type="checkbox" id="rememberMe" />
-              <label htmlFor="rememberMe">Ghi nhớ mật khẩu</label>
-            </div>
-          </form>
+      <div className="login-page">
+        <div className="login-container">
+          <div className="login-box">
+            <button className="back-button">←</button>
+            <h2>ĐĂNG NHẬP</h2>
+            <form>
+              <div className="input-group">
+                <input
+                  type="text"
+                  placeholder="Tên đăng nhập"
+                  className="input-field"
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type={passwordVisible ? "text" : "password"}
+                  placeholder="Mật khẩu"
+                  className="input-field"
+                />
+                <button
+                  type="button"
+                  className="toggle-password"
+                  onClick={togglePasswordVisibility}
+                ></button>
+              </div>
+              <button className="login-button" type="submit">
+                ĐĂNG NHẬP
+              </button>
+              <div className="remember">
+                <input type="checkbox" id="rememberMe" />
+                <label htmlFor="rememberMe">Ghi nhớ mật khẩu</label>
+              </div>
+            </form>
+          </div>
+          <div className="logo-section">
+            <img
+              src="https://pakdd.org/archive/pakdd2015/images/543px-Logo-hcmut.svg.png"
+              alt="BK Logo"
+              className="logo"
+            />
+            <h1>SMART PRINTING SYSTEM</h1>
+          </div>
         </div>
-        <div className="logo-section">
-          <img
-            src="https://pakdd.org/archive/pakdd2015/images/543px-Logo-hcmut.svg.png"
-            alt="BK Logo"
-            className="logo"
-          />
-          <h1>SMART PRINTING SYSTEM</h1>
-        </div>
+        <div className="SigninFooter">Secured by HCMUT - SPSO system</div>
       </div>
-      <footer>Secured by HCMUT - SPSO system</footer>
     </>
   );
 };
